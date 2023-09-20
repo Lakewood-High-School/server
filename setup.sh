5b4c2cd8c9b19e0e664914362f907312e3036b62
Luna Kilborn <leland.kilborn@hotmail.com>
2023-09-20 18:02:41 -0400

    Modify setup.sh and remove submodules
    The submodule stuff just wasn't really working, so I am switching to a
    different strategy: just cloning the repo into an ignored folder.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 echo Setting up lakewood.ai server
+mkdir subs
+echo Cloning frontend
+cd subs
+git clone https://github.com/Lakewood-High-School/lakewood.ai
+cd ..
+echo Cloned frontend
 echo Running hosting setup file
 ./subs/lakewood.ai/setup.sh
 echo Sever setup completed
